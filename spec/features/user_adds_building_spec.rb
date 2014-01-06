fab1e6c76bdfbd250f9289cc519a03c45b41b8ce
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-05 22:28:17 -0500

    add happy path new building feature spec

---
+++ spec/features/user_adds_building_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+feature "Record a new building", %q{
+  As a real estate associate
+  I want to record a building
+  So that I can refer back to pertinent information
+  } do
+
+    # Acceptance Criteria:
+
+    # I must specify a street address, city, state, and postal code
+    # Only US states can be specified
+    # I can optionally specify a description of the building
+    # If I enter all of the required information in the required format, the building is recorded.
+    # If I do not specify all of the required information in the required formats, the building is not recorded and I am presented with errors
+    # Upon successfully creating a building, I am redirected so that I can record another building.
+
+    context 'user provides relevent information' do
+      it 'creates a new building' do
+        count = Building.all.count
+        street_address = '1116 Dorchester Ave'
+        city = 'Boston'
+        postal_code = '02125'
+        state = 'MA'
+
+        visit new_building_path
+
+        fill_in "Street Address", with: street_address
+        fill_in "City", with: city
+        fill_in "Postal Code", with: postal_code
+        select state, from: "State"
+        click_on "Create New Building"
+
+        expect(Building.all.count).to eql(count + 1)
+        current_path.should eq(new_building_path)
+        expect(page).to have_content "Create a New Building"
+      end
+    end
+
+    # context 'user provides incomplete information' do
+
+    # end
+
+  end
