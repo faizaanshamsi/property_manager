abfb89a05e9c9d814940a1a7cc411e89a64d527a
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-06 02:12:01 -0500

    add migration for owner id in building

---
+++ db/migrate/20140106070726_add_owner_to_building.rb
@@ -0,0 +1,9 @@
+class AddOwnerToBuilding < ActiveRecord::Migration
+  def up
+    add_column :buildings, :owner_id, :integer
+  end
+
+  def down
+    remove_column :buildings, :owner_id
+  end
+end
