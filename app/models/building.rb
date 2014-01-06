62d55df8d866a9cf082ab7151db64a90f81fd0d3
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-06 00:21:20 -0500

    refactor building class state arrary

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,14 +1,11 @@
 class Building < ActiveRecord::Base
-  STATE = %w{ AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY }
+  def self.state
+    %w{ AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY }
+  end
 
   validates :name, length: { minimum: 1 }
   validates :street_address, length: { minimum: 1 }
   validates :city, length: { minimum: 1 }
   validates :postal_code, length: { minimum: 1 }
-  validates_inclusion_of :state, in: STATE
-
-  def self.state
-    %w{ AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY }
-  end
-
+  validates_inclusion_of :state, in: self.state
 end
